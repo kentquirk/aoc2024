34310694d4c02b7e6abfa2d764b5aeffab87f0e3
Kent Quirk <kentquirk@honeycomb.io>
2024-12-08 14:51:06 -0500

    day 8, part 2

--- day08_go/main.go
+++ day08_go/main.go
@@ -47,9 +47,9 @@ func parseAntennaMap(lines []string) *antennaMap {
 	return m
 }
 
-// returns a slice of positions that are antinodes for the two given positions
+// returns a slice of positions that are part1Antinodes for the two given positions
 // but only if they're on the map (so it may return 0, 1, or 2 positions)
-func (m *antennaMap) antinodes(p1, p2 position) []position {
+func (m *antennaMap) part1Antinodes(p1, p2 position) []position {
 	antinodes := []position{}
 	a1 := p2.add(p1.vectorTo(p2))
 	if m.isOnMap(a1) {
@@ -62,15 +62,30 @@ func (m *antennaMap) antinodes(p1, p2 position) []position {
 	return antinodes
 }
 
-func part1(lines []string) int {
-	antennaMap := parseAntennaMap(lines)
+// returns a slice of positions that are part2Antinodes for the two given positions
+// but only if they're on the map (so assuming that the two positions are on the map,
+// it will return at least 2 or possibly many more positions)
+func (m *antennaMap) part2Antinodes(p1, p2 position) []position {
+	antinodes := []position{p1, p2}
+	d1 := p1.vectorTo(p2)
+	for a1 := p2.add(d1); m.isOnMap(a1); a1 = a1.add(d1) {
+		antinodes = append(antinodes, a1)
+	}
+	d2 := p2.vectorTo(p1)
+	for a2 := p1.add(d2); m.isOnMap(a2); a2 = a2.add(d2) {
+		antinodes = append(antinodes, a2)
+	}
+	return antinodes
+}
+
+func bothParts(am *antennaMap, f func(p1, p2 position) []position) int {
 	allNodes := map[position]struct{}{}
 	count := 0
-	for freq, positions := range antennaMap.m {
+	for freq, positions := range am.m {
 		fcount := 0
 		for i, p1 := range positions {
 			for _, p2 := range positions[i+1:] {
-				antinodes := antennaMap.antinodes(p1, p2)
+				antinodes := f(p1, p2)
 				count += len(antinodes)
 				fcount += len(antinodes)
 				for _, a := range antinodes {
@@ -83,10 +98,6 @@ func part1(lines []string) int {
 	fmt.Printf("Total: %d\n", count)
 	fmt.Printf("Unique: %d\n", len(allNodes))
 	return len(allNodes)
-}
-
-func part2(lines []string) int {
-	return 0
 }
 
 func readlines(filename string) []string {
@@ -108,5 +119,7 @@ func main() {
 		filename = args[0]
 	}
 	lines := readlines(filename)
-	fmt.Println(part1(lines))
+	am := parseAntennaMap(lines)
+	fmt.Println(bothParts(am, am.part1Antinodes))
+	fmt.Println(bothParts(am, am.part2Antinodes))
 }
