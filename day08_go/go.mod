854a2b7f5cdfbe87c2793c162e82f4b179ea98a2
Kent Quirk <kentquirk@honeycomb.io>
2024-12-08 14:35:07 -0500

    day 8, part 1

---
+++ day08_go/go.mod
@@ -0,0 +1,3 @@
+module github.com/kentquirk/aoc2024/day08
+
+go 1.23
---
+++ day08_go/main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+type position struct {
+	r int
+	c int
+}
+
+func (p position) vectorTo(p2 position) position {
+	return position{p2.r - p.r, p2.c - p.c}
+}
+
+func (p position) add(p2 position) position {
+	return position{p.r + p2.r, p.c + p2.c}
+}
+
+type antennaMap struct {
+	w int
+	h int
+	m map[byte][]position
+}
+
+func (a *antennaMap) isOnMap(p position) bool {
+	return p.r >= 0 && p.r < a.h && p.c >= 0 && p.c < a.w
+}
+
+// record a slice of positions for each frequency
+func parseAntennaMap(lines []string) *antennaMap {
+	m := &antennaMap{w: len(lines[0]), h: len(lines), m: make(map[byte][]position)}
+	for r, line := range lines {
+		for c, char := range line {
+			switch char {
+			case '.':
+				// do nothing
+			default:
+				m.m[byte(char)] = append(m.m[byte(char)], position{r, c})
+			}
+		}
+	}
+	return m
+}
+
+// returns a slice of positions that are antinodes for the two given positions
+// but only if they're on the map (so it may return 0, 1, or 2 positions)
+func (m *antennaMap) antinodes(p1, p2 position) []position {
+	antinodes := []position{}
+	a1 := p2.add(p1.vectorTo(p2))
+	if m.isOnMap(a1) {
+		antinodes = append(antinodes, a1)
+	}
+	a2 := p1.add(p2.vectorTo(p1))
+	if m.isOnMap(a2) {
+		antinodes = append(antinodes, a2)
+	}
+	return antinodes
+}
+
+func part1(lines []string) int {
+	antennaMap := parseAntennaMap(lines)
+	allNodes := map[position]struct{}{}
+	count := 0
+	for freq, positions := range antennaMap.m {
+		fcount := 0
+		for i, p1 := range positions {
+			for _, p2 := range positions[i+1:] {
+				antinodes := antennaMap.antinodes(p1, p2)
+				count += len(antinodes)
+				fcount += len(antinodes)
+				for _, a := range antinodes {
+					allNodes[a] = struct{}{}
+				}
+			}
+		}
+		fmt.Printf("Frequency %c: %d\n", freq, fcount)
+	}
+	fmt.Printf("Total: %d\n", count)
+	fmt.Printf("Unique: %d\n", len(allNodes))
+	return len(allNodes)
+}
+
+func part2(lines []string) int {
+	return 0
+}
+
+func readlines(filename string) []string {
+	f, err := os.Open(fmt.Sprintf("./data/%s.txt", filename))
+	if err != nil {
+		log.Fatal(err)
+	}
+	b, err := io.ReadAll(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return strings.Split(string(b), "\n")
+}
+
+func main() {
+	args := os.Args[1:]
+	filename := "sample"
+	if len(args) > 0 {
+		filename = args[0]
+	}
+	lines := readlines(filename)
+	fmt.Println(part1(lines))
+}
